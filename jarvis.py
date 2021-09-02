ae17982a9fa2a69953fdf0bf07c4a3f91f6c99b4
tawhidmonowar <tawhidmonowar@gmail.com>
2021-09-03 00:50:58 +0600

    03-09-2021

--- jarvis.py
+++ jarvis.py
@@ -5,7 +5,6 @@ import wikipedia
 
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
-# print(voices[0].id)
 engine.setProperty('voice', voices[0].id)
 
 
@@ -16,23 +15,18 @@ def speak(audio):
 
 def wishMe():
     hour = int(datetime.datetime.now().hour)
-
     if 0 <= hour < 12:
         speak("Good Morning! Let's get some coffee and start grinding")
-
     elif 12 <= hour < 15:
         speak("Good Afternoon, You are doing great")
-
     elif 15 <= hour < 20:
         speak("Good Evening. it's time for some tea")
-
     else:
         speak("Get recharged now. Good Night")
-
     speak("I am Jarvis at your service. how can i help you?")
 
 
-def commend():
+def command():
     listener = sr.Recognizer()
     try:
         with sr.Microphone() as source:
@@ -41,22 +35,23 @@ def commend():
             query = listener.recognize_google(voice)
             query = query.lower()
             print("User said:" + query)
+            if "wikipedia" in query:
+                searchWiki(query)
     except:
         engine.say('Say that again...')
         engine.runAndWait()
-        commend()
+        command()
+
+
+def searchWiki(searchQuery):
+    speak("Searching wikipedia...")
+    cmd = searchQuery.replace("wikipedia", "")
+    results = wikipedia.summary(cmd, sentences=2)
+    speak("According to wikipedia...")
+    print(results)
+    speak(results)
 
 
 if __name__ == '__main__':
     wishMe()
-    # logic for executing task in command
-    while True:
-        if "wikipedia" in query:
-            speak("Searching wikipedia...")
-            cmd = query.replace("wikipedia", "")
-            results = wikipedia.summary(query, sentences=2)
-            speak("According to wikipedia...")
-            print(results)
-            speak(results)
-
-
+    command()
